5d6b1db0e6d6f8f5aac4b2692ad158773cd831f0
kais-radwan <kais.radwan.personal@gmail.com>
2025-03-26 21:01:44 +0200

    responsive improvements on figma section

--- src/components/new/figma.tsx
+++ src/components/new/figma.tsx
@@ -38,33 +38,33 @@ export function NewFigma() {
 
         <div className="w-full flex flex-col items-center justify-center h-full z-20 cursor-default relative overflow-hidden p-2">
           <div
-            className="w-full bg-black rounded-3xl h-[26.438rem] border border-[#2D2D2D] relative pl-[10rem] flex items-center justify-center z-10"
+            className="w-full bg-black rounded-3xl h-[26.438rem] border border-[#2D2D2D] relative pl-[7rem] md:pl-[10rem] flex items-center justify-center z-10 px-4 md:px-0"
             style={{
               boxShadow: "0px 0px 0px 6px #121212, 0px 0px 0px 7px #2f2f2f",
             }}
           >
-            <div className="absolute p-4 top-0 left-0 text-[12px] rounded-l-3xl bg-[#131313] border-r border-white/10 h-full w-[10rem] flex flex-col gap-6">
+            <div className="absolute p-2 pt-4 md:p-4 top-0 left-0 text-[9px] md:text-[12px] rounded-l-3xl bg-[#131313] border-r border-white/10 h-full w-[6rem] md:w-[10rem] flex flex-col gap-6">
               <Figma size={32} className="opacity-70" />
               <div className="flex flex-col w-full gap-3">
                 <div className="opacity-60">Layers</div>
                 <div className="flex flex-col w-full gap-2">
-                  <div className="p-2 py-1 w-full bg-blue-400/10 rounded-lg flex items-center gap-2">
-                    <Frame size={14} />
-                    <div>Landing Page</div>
+                  <div className="p-2 py-1 overflow-hidden w-full bg-blue-400/10 rounded-lg flex items-center gap-2">
+                    <Frame size={14} className="hidden md:flex" />
+                    <div className="min-w-max">Landing Page</div>
                   </div>
 
                   <div className="p-2 py-1 w-full rounded-lg flex items-center gap-2">
-                    <Frame size={14} />
+                    <Frame size={14} className="hidden md:flex" />
                     <div>Blog Page</div>
                   </div>
 
                   <div className="p-2 py-1 w-full rounded-lg flex items-center gap-2">
-                    <Frame size={14} />
+                    <Frame size={14} className="hidden md:flex" />
                     <div>Pricing Page</div>
                   </div>
 
                   <div className="p-2 py-1 w-full rounded-lg flex items-center gap-2">
-                    <Frame size={14} />
+                    <Frame size={14} className="hidden md:flex" />
                     <div>Signup Page</div>
                   </div>
                 </div>
@@ -96,7 +96,13 @@ export function NewFigma() {
           >
             <source src="/beam-1.webm" />
           </video>
-          <video autoPlay playsInline loop muted className="-mt-36 md:-mt-56 z-0 w-full min-w-[120%] md:min-w-full">
+          <video
+            autoPlay
+            playsInline
+            loop
+            muted
+            className="-mt-36 md:-mt-56 z-0 w-full min-w-[120%] md:min-w-full"
+          >
             <source src="/beam-1.webm" />
           </video>
 
@@ -119,7 +125,11 @@ export function NewFigma() {
               ></div>
             </div>
 
-            <p>https://<span className="text-[#484848] font-bold">your-project</span>.blackbox.ai</p>
+            <p className="text-xs md:text-base">
+              https://
+              <span className="text-[#484848] font-bold">your-project</span>
+              .blackbox.ai
+            </p>
           </div>
         </div>
       </div>
